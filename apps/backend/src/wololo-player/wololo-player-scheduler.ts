d7f4ec233a21d8ef97e07348d84be9c58f4bcb61
AlexDissaux <dissauxalex@gmail.com>
2026-09-01 10:00:09 +0200

    disale wololo player sheduler CRON

--- apps/backend/src/wololo-player/wololo-player-scheduler.ts
+++ apps/backend/src/wololo-player/wololo-player-scheduler.ts
@@ -8,12 +8,12 @@ export class WololoPlayerScheduler {
 
   constructor(private readonly wololoPlayerService: WololoPlayerService) {}
 
-  @Cron('*/4 * * * *')
-  async handleWololoPlayersSync(): Promise<void> {
-    try {
-      await this.wololoPlayerService.syncWololoPlayers();
-    } catch (error) {
-      this.logger.error('Scheduled wololo players sync failed:', error);
-    }
-  }
+  // @Cron('*/4 * * * *')
+  // async handleWololoPlayersSync(): Promise<void> {
+  //   try {
+  //     await this.wololoPlayerService.syncWololoPlayers();
+  //   } catch (error) {
+  //     this.logger.error('Scheduled wololo players sync failed:', error);
+  //   }
+  // }
 }
