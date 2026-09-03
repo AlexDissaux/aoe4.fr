78f0f44850eebdde9f20e09c81d052ca56d2e147
AlexDissaux <dissauxalex@gmail.com>
2026-09-03 14:25:50 +0200

    using wololo games

--- apps/wololo-challenge/src/index.ts
+++ apps/wololo-challenge/src/index.ts
@@ -4,15 +4,8 @@ export { default as App } from './App';
 
 // Export components
 export { Header } from './components/Header';
-export { default as Countdown } from './components/Countdown';
 export { default as Podium } from './components/podium/Podium';
 
 // Export hooks
 export { usePlayers } from './hook/usePlayers';
 export { useTeams } from './hook/useTeams';
-
-// Export services
-export * from './api/player.service';
-
-// Export data
-export * from './db/data';
