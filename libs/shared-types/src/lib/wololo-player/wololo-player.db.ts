2ca00b091390250522ff0764d53b2ab9a2e1bf72
AlexDissaux <dissauxalex@gmail.com>
2026-09-06 12:32:35 +0200

    modifying player

--- libs/shared-types/src/lib/wololo-player/wololo-player.db.ts
+++ libs/shared-types/src/lib/wololo-player/wololo-player.db.ts
@@ -183,7 +183,7 @@ INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (5590088, '3M', 
 
 INSERT INTO wololo_teams (id, name, color) VALUES ('DDC', 'dynastie du camion', 'stone');
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (4314006, 'DDC', true);
-INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (25250655, 'DDC', false);
+INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (6094081, 'DDC', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (1849651, 'DDC', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (3953084, 'DDC', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (20497467, 'DDC', false);
@@ -210,7 +210,7 @@ INSERT INTO wololo_teams (id, name, color) VALUES ('GIRLS', 'Who run the world ?
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (6492127, 'GIRLS', true);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (14697531, 'GIRLS', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (6552004, 'GIRLS', false);
-INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (8824061, 'GIRLS', false);
+INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (11763649, 'GIRLS', false);
 
 INSERT INTO wololo_teams (id, name, color) VALUES ('VES', 'Volda Esport', 'purple-dark');
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (19131565, 'VES', true);
@@ -310,3 +310,6 @@ INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (1969458, 'RM', 
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (9212805, 'RM', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (24165425, 'RM', false);
 INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (25755296, 'RM', false);
+
+INSERT INTO wololo_players (profile_id, team_id, is_cap) VALUES (8953699, 'OURS', false);
+
