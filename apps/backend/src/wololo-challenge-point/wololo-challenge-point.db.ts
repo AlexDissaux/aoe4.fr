5b6c07d21c6c93dd8b2dedfefa0230b4ddb1cb69
AlexDissaux <dissauxalex@gmail.com>
2026-09-10 10:52:46 +0200

    adding points multiplex

--- apps/backend/src/wololo-challenge-point/wololo-challenge-point.db.ts
+++ apps/backend/src/wololo-challenge-point/wololo-challenge-point.db.ts
@@ -1,3 +1,31 @@
+
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1712108, 0, 'Stay tuned to take part in various challenges and score points in this category.');
 
+
+
 DELETE FROM wololo_challenge_points WHERE profile_id = 1712108;
+
+
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1270139,	5, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1421448,	5, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1712108,	5, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (773023,	5, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (17667530,	5, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (23160801,	4, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (3380403,	4, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1091161,	4, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24115216,	4, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (3237575,	4, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (12029658,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (132424,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (863196,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6489303,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (22099089,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1571336,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2137505,	3, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24748120,	2, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2607134,	2, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (4314006,	2, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (843656,	2, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (19131565,	2, 'MultiPlex');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (25106587,	1, 'MultiPlex');
