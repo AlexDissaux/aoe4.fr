94535868d771470faece8b6fb6b02eb2409e54a8
Alexandre Dissaux <dissauxalex@example.com>
2026-09-20 12:16:29 +0200

    wololo challenge points crucible + wololoCup

--- apps/backend/src/wololo-challenge-point/wololo-challenge-point.db.ts
+++ apps/backend/src/wololo-challenge-point/wololo-challenge-point.db.ts
@@ -38,17 +38,34 @@ INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24544167
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11575115, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (15289018, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (4350135, 1, 'Crucible');
-INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1421448, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2103888, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6964911, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (10351369, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11351113, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2137505, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (7964132, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11960419, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6489303, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (3877183, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8175120, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (669640, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (9644505, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (23387649, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (23160801, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (12061224, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2672597, 1, 'Crucible');
 INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8250217, 1, 'Crucible');
-
-
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (12029658, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (790303, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (17594012, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (863196, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8889679, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (4231596, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8275065, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6946414, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (22099089, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8800916, 1, 'Crucible');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1260486, 1, 'Crucible');
 
 
 
@@ -69,40 +86,25 @@ INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24115216
 
 
 
-
-
-
-
-
-
-
-
-
-
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (26086942, 1, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11960419, 1, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (773023, 5, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (9644505, 1, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (23160801, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (15289018, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (19869372, 1, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (4350135, 5, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11575115, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2103888, 5, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (7219954, 1, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (22968308, 4, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (18770984, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11351113, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (14414284, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (7090781, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6964911, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6592588, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2137505, 5, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1336862, 3, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (769187, 4, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24062589, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (790303, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8250217, 2, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (25145039, 3, 'Rising Empires');
-// INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8415007, 2, 'Rising Empires');
-	
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (24115216, 1, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8800916, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1421448, 6, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (10351369, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (10605002, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (12029658, 4, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (20837980, 5, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11575115, 3, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (19869372, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6924135, 4, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (11960419, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (4703134, 4, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (23160801, 1, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (1712108, 3, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (21170971, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (15289018, 1, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6489303, 1, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2607134, 0, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (2137505, 5, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (9644505, 4, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (8250217, 2, 'WololoCup_3');
+INSERT INTO wololo_challenge_points (profile_id, points, label) VALUES (6964911, 0, 'WololoCup_3');
